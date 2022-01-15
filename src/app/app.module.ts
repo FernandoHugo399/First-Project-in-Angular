719d1d91ec133cadfcfbb28febb294d9c74b4238
Hugo Fernando <huginho888@outlook.com>
2022-01-15 13:51:46 -0300

    Reestruturando projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { MainComponent } from './components/main/main.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './views/home/home.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
